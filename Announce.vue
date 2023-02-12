d074fce12ecbb039dc92e8f63024f2daa5259305
LilPCoding <119925017+LilPCoding@users.noreply.github.com>
2023-02-12 22:07:08 +0700

    Add files via upload

---
+++ Announce.vue
@@ -0,0 +1,223 @@
+<template>
+    
+      <!-- Start Banner Hero -->
+    <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-bs-target="#template-mo-zay-hero-carousel"  data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#template-mo-zay-hero-carousel"  data-bs-slide-to="1"></li>
+            <li data-bs-target="#template-mo-zay-hero-carousel"  data-bs-slide-to="2"></li>
+            <li data-bs-target="#template-mo-zay-hero-carousel"  data-bs-slide-to="3"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="container">
+                    <div class="row p-5">
+                        <div class="mx-auto">
+                            <img class="img-fluid" src="https://www.mju.ac.th/en/images/webometrics_promote_EN.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row p-5">
+                        <div class="mx-auto">
+                            <img class="img-fluid" src="https://www.mju.ac.th/en/images/bn_go-eco.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row p-5">
+                        <div class="mx-auto">
+                            <img class="img-fluid" src="https://www.mju.ac.th/en/images/go-green.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="container">
+                    <div class="row p-5">
+                        <div class="mx-auto">
+                            <img class="img-fluid" src="https://www.mju.ac.th/en/images/core-value.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <a class="carousel-control-prev text-decoration-none w-auto ps-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="prev">
+            <i class="fas fa-chevron-left"></i>
+        </a>
+        <a class="carousel-control-next text-decoration-none w-auto pe-3" href="#template-mo-zay-hero-carousel" role="button" data-bs-slide="next">
+            <i class="fas fa-chevron-right"></i>
+        </a>
+    </div>
+    <!-- End Banner Hero -->
+
+    <!-- Start Categories of The Month -->
+    <section class="container py-5">
+        <div class="row text-center pt-3">
+            <div class="col-lg-6 m-auto">
+                <h1 class="h1">Announce</h1>
+                <hr>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-4 p-5 mt-3">
+                <a href="https://admissions.mju.ac.th/www/"><img src="https://www.mju.ac.th/en/images/pic1.png" height="344px" width="344px" class="rounded-circle img-fluid border"></a>
+                <h5 class="text-center mt-3 mb-3">Bachelor's degree</h5>
+                <hr>
+            </div>
+            <div class="col-12 col-md-4 p-5 mt-3">
+                <a href="http://www.grad.mju.ac.th/apply/"><img src="https://www.mju.ac.th/en/images/2%20copy.png" height="344px" width="344px" class="rounded-circle img-fluid border"></a>
+                <h2 class="h5 text-center mt-3 mb-3">Master's degree</h2>
+                <hr>
+            </div>
+            <div class="col-12 col-md-4 p-5 mt-3">
+                <a href="http://www.grad.mju.ac.th/apply/" target="_blank"><img src="https://www.mju.ac.th/en/images/Stu3.png" height="344px" width="344px" class="rounded-circle img-fluid border"></a>
+                <h2 class="h5 text-center mt-3 mb-3">Doctoral degree</h2>
+                <hr>
+            </div>
+        </div>
+    </section>
+    <!-- End Categories of The Month -->
+
+    <!-- Start Featured Product -->
+    <section class="bg-light">
+        <div class="container py-5">
+            <div class="row text-center py-3">
+                <div class="col-lg-6 m-auto">
+                    <h1 class="h1">News and Activities</h1>
+                    <hr>
+                </div>
+            </div>
+            <div class="row">
+                <!-- card 1 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card h-100">
+                        <a href="https://erp.mju.ac.th/informationDetail.aspx?newsId=4995&lang=en" target="_blank">
+                            <img src="https://erp.mju.ac.th/picture/information/8eed863f1f854a6494270633c689e00as.jpg" class="card-img-top" alt="...">
+                        </a>
+                        <div class="card-body">
+                            <a href="https://erp.mju.ac.th/informationDetail.aspx?newsId=4995&lang=en" target="_blank" class="h2 text-decoration-none text-dark">Visited Maejo University</a>
+                            <p class="card-text">
+                                 Vice President of Legislative Yuan, Taiwan (R.O.C) and President of National Chung Hsing University Global Alumni Association together with Representative of Taipei Economic and Cultural Office in Thailand and the company visited Maejo University.
+                            </p>
+                            <p class="text-muted">30 January 2023</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- card 2 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card h-100">
+                        <a href="https://erp.mju.ac.th/informationDetail.aspx?newsId=4966&lang=en" target="_blank">
+                            <img src="https://erp.mju.ac.th/picture/information/dec5b6c2cfac482f87e6fdcdeb67f8abs.jpg" class="card-img-top" alt="...">
+                        </a>
+                        <div class="card-body">
+                            <a href="https://erp.mju.ac.th/informationDetail.aspx?newsId=4966&lang=en" target="_blank" class="h2 text-decoration-none text-dark">Architecture</a>
+                            <p class="card-text">
+                                  Students from Faculty of Architecture and Environmental Design took a study tour about professional practices in architecture.
+                            </p>
+                            <p class="text-muted">27 December 2022</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- card 3 -->
+                <div class="col-12 col-md-4 mb-4">
+                    <div class="card h-100">
+                        <a href="https://erp.mju.ac.th/informationDetail.aspx?newsId=4920&lang=en" target="_blank">
+                            <img src="https://erp.mju.ac.th/picture/information/628ad9c4a10c4674be7a73dc27f40a3ds.jpg" class="card-img-top" alt="...">
+                        </a>
+                        <div class="card-body">
+                            <a href="https://erp.mju.ac.th/informationDetail.aspx?newsId=4920&lang=en" target="_blank" class="h2 text-decoration-none text-dark">MIAT 2022</a>
+                            <p class="card-text">
+                                  Maejo Innovation of Agricultural Technology Fair and Open Farm 2022 (MIAT 2022)
+                            </p>
+                            <p class="text-muted">07 December 2022</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Featured Product -->
+
+
+
+    <!-- Start Footer -->
+    <footer class="bg-dark" id="tempaltemo_footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-success border-bottom pb-3 border-light logo">MJU</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li>
+                            <i class="fas fa-map-marker-alt fa-fw"></i>
+                            No. 63 Moo 4, Nong Han Subdistrict, San Sai District, Chiang Mai Province 50290
+                        </li>
+                        <li>
+                            <i class="fa fa-phone fa-fw"></i>
+                            <a class="text-decoration-none" href="tel:053-873000"> 053-873000</a>
+                        </li>
+                        <li>
+                            <i class="fa fa-envelope fa-fw"></i>
+                            <a class="text-decoration-none" href="mailto:maejo@mju.ac.th"> maejo@mju.ac.th</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">News and Activities</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none" href="https://erp.mju.ac.th/informationDetail.aspx?newsId=4995&lang=en" target="_blank">Visited Maejo University</a></li>
+                        <li><a class="text-decoration-none" href="https://erp.mju.ac.th/informationDetail.aspx?newsId=4966&lang=en" target="_blank">Architecture</a></li>
+                        <li><a class="text-decoration-none" href="https://erp.mju.ac.th/informationDetail.aspx?newsId=4920&lang=en" target="_blank">MIAT 2022</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Further Info</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none" href="#/Announce">Home</a></li>
+                        <li><a class="text-decoration-none" href="#/Announce">Announce</a></li>
+                        <li><a class="text-decoration-none" href="#/Subjects">List of subjects</a></li>
+                        <li><a class="text-decoration-none" href="#/AddDrop_Subjects">Add/Drop courses</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="row text-light mb-4">
+                <div class="col-12 mb-3">
+                    <div class="w-100 my-3 border-top border-light"></div>
+                </div>
+                <div class="col-auto me-auto">
+                    <ul class="list-inline text-left footer-icons">
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="#"><i class="fab fa-facebook-f fa-lg fa-fw"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="#"><i class="fab fa-instagram fa-lg fa-fw"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="#"><i class="fab fa-twitter fa-lg fa-fw"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="#"><i class="fab fa-linkedin fa-lg fa-fw"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-100 bg-black py-3">
+            
+        </div>
+
+    </footer>
+    <!-- End Footer -->
+    
+</template>
